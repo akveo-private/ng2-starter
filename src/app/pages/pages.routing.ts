3cedeb158fa9b494653798d6de0a069ca02746e9
Dmitry Nehaychik <4dmitr@gmail.com>
2016-10-21 15:04:53 +0300

    feat(app): utils and auth modules

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,18 +1,23 @@
 import { Routes, RouterModule }  from '@angular/router';
 import { Pages } from './pages.component';
+import { AuthGuard } from '../auth/auth.guard';
 // noinspection TypeScriptValidateTypes
+
 const routes: Routes = [
   {
     path: 'login',
+    canActivate: [AuthGuard],
     loadChildren: () => System.import('./login/login.module')
   },
   {
     path: 'register',
+    canActivate: [AuthGuard],
     loadChildren: () => System.import('./register/register.module')
   },
   {
     path: 'pages',
     component: Pages,
+    canActivate: [AuthGuard],
     children: [
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: 'dashboard', loadChildren: () => System.import('./dashboard/dashboard.module') },
